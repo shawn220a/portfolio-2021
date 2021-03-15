11ce7eaccb325424be2321ad9171be865be7c1c4
shawn220a <shawn.42496@gmail.com>
2021-03-15 17:17:36 -0500

    Create contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,20 +7,22 @@ export const Contact = () => {
     <div>
       <h1>Contact</h1>
       <Form>
-        <Form.Group controlId='formBasicEmail'>
+        <Form.Group>
+          <Form.Label>Name</Form.Label>
+          <Form.Control type='text' placeholder='Name' required />
+        </Form.Group>
+        <Form.Group>
           <Form.Label>Email address</Form.Label>
-          <Form.Control type='email' placeholder='Enter email' />
-          <Form.Text className='text-muted'>
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Control type='email' placeholder='Enter email' required />
         </Form.Group>
-
-        <Form.Group controlId='formBasicPassword'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control type='password' placeholder='Password' />
-        </Form.Group>
-        <Form.Group controlId='formBasicCheckbox'>
-          <Form.Check type='checkbox' label='Check me out' />
+        <Form.Group controlId='exampleForm.ControlTextarea1'>
+          <Form.Label>Message</Form.Label>
+          <Form.Control
+            as='textarea'
+            rows={3}
+            required
+            placeholder='Your message here ...'
+          />
         </Form.Group>
         <Button variant='primary' type='submit'>
           Submit
