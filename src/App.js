3ee9d65c5b37c0f428c69d4f880cafb1d02aa538
shawn220a <shawn.42496@gmail.com>
2021-03-15 16:39:01 -0500

    Create contact component and add header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { About } from './components/About';
+import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Portfolio } from './components/Portfolio';
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <About />
       <Portfolio />
+      <Contact />
       <Footer />
     </div>
   );
