63d723982420962acadbc42db698e5a0a704a0f2
shawn220a <shawn.42496@gmail.com>
2021-03-15 19:18:24 -0500

    Add two buttons to the portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -16,16 +16,6 @@ export const Portfolio = () => {
               bulk of the card's content.
             </Card.Text>
             <Button variant='primary'>Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant='top' src='holder.js/100px180' />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
             <Button variant='primary'>Go somewhere</Button>
           </Card.Body>
         </Card>
@@ -37,6 +27,19 @@ export const Portfolio = () => {
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </Card.Text>
+            <Button variant='primary'>Go somewhere</Button>
+            <Button variant='primary'>Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant='top' src='holder.js/100px180' />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant='primary'>Go somewhere</Button>
             <Button variant='primary'>Go somewhere</Button>
           </Card.Body>
         </Card>
